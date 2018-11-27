9c63413fbc1afd0cf918664c3b1037f5acb02751
KaranGrover912 <grover.k912@gmail.com>
2018-11-27 13:15:30 +0800

    ADD: login screen

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import Login from './views/Login/index';
+
+const App = () => (
+			<div>
+				<Login/>
+			</div>
+);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import App from './App';
+import './styles/index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<h1> Initial app </h1>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
